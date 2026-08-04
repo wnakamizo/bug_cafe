5ada8fec04714c0f1ea9a09b959a32548b11996a
wnakamizo <w.nakamizo@genbasupport.com>
2026-08-04 16:34:18 +0900

    Rename variables for clarity

--- cafe.rb
+++ cafe.rb
@@ -24,9 +24,9 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drinks_order = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+foods_order = take_order(FOODS)
+total = DRINKS[drinks_order][:price].to_i + FOODS[foods_order][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
